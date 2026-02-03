f8d4bc5845b4599cdb98b7ff744f66ee9bd00278
Sebastian Elliott Pedrosa <pedrosaenterprises@gmail.com>
2026-02-03 14:32:08 -0400

    Revert "Sucessfully added local llm support through picollm. To do: speech to text and text to speech."
    This reverts commit 78479beb3bde60f9ccbd20fe68aac0673402d42a.

--- jarvis.py
+++ jarvis.py
@@ -1,7 +1,6 @@
 import pvporcupine
 import os
 from pvrecorder import PvRecorder
-import picollm
 def main():
     print("Starting Jarvis voice assistant...")
     porcupine, recorder = initialize_wake_word_detection()
@@ -10,18 +9,6 @@ def main():
         keyword_index = porcupine.process(audio_frame)
         if keyword_index == 0:
             print ("detected `jarvis`")
-            pllm = initLLM()
-            if pllm:
-                #todo: add conversation history
-                #todo: add speech to text
-                #todo: add text to speech
-                #todo: add context awareness maybe?
-                #todo: add more advanced prompt engineering
-                #todo intent recognition for changing models
-                res = pllm.generate(prompt='You are Jarvis, a helpful AI assistant. Introduce yourself briefly.')
-                print(res.completion)
-                #print("Jarvis has been summoned.")
-
         elif keyword_index == 1:
             print ("detected `bumblebee`")
     
@@ -34,7 +21,7 @@ def initialize_wake_word_detection():
       access_key= os.environ.get("ACCESS_KEY"),
       keywords=['jarvis', 'bumblebee']
     )
-    #make this a start recording function, and make a stop recording function
+    #make this a start recoeding function, and make a stop recording function
     recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
     recorder.start()
     return porcupine, recorder
@@ -42,13 +29,5 @@ def initialize_wake_word_detection():
 
 def get_next_audio_frame(recorder):
   return recorder.read()
-
-
-
-def initLLM():
-    pllm = picollm.create(
-    access_key = os.environ.get("ACCESS_KEY"),
-    model_path ='llama-3.2-3b-instruct-505.pllm')
-    return pllm
 
 main()
